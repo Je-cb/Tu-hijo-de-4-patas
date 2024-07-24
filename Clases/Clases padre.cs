858f952246d8a52479177c63c08bcd73d0e44987
Jecb_ <Jecb_@BIRDPC>
2024-07-24 12:13:13 -0400

    Agregar archivos de proyecto.

---
+++ Clases/Clases padre.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Clases
+{
+    //Clase padre persona
+    internal class Persona
+    {
+        //Atributos
+        private string Nombre;
+        private string Apellido;
+        private int ID;
+
+        //Metodos
+
+        //Constructor parametrico
+        public Persona(string Nombre, string Apellido, int ID)
+        {
+            this.Nombre = Nombre;
+            this.Apellido = Apellido;
+            this.ID = ID;
+        }
+
+        //Informacion del usuario
+        public void InfoPersona()
+        {
+            string info = $"********************INFORMACIÓN DE USUARIO***********************" + Environment.NewLine +
+                          $"Nombre: {Nombre}" + Environment.NewLine +
+                          $"Apellido: {Apellido}" + Environment.NewLine +
+                          $"Número de identificación: {ID}" + Environment.NewLine;
+
+            Console.WriteLine(info);
+        }
+    }
+
+    //Clase padre producto
+    internal class Producto
+    {
+        //Atributos
+        private int Codigo;
+        private string Nombre;
+        private string Descripcion;
+        private float Precio;
+        private string Categoria;
+
+    }
+}
---
+++ Clases/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Clases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserte los datos del usuario:" + Environment.NewLine + "Nombre:" + Environment.NewLine);
+
+            string Nombre = Console.ReadLine();
+
+            Console.WriteLine("Apellido:" + Environment.NewLine);
+
+            string Apellido = Console.ReadLine();
+
+            Console.WriteLine("Numero de identificacion" + Environment.NewLine);
+
+            int ID = Convert.ToInt32(Console.ReadLine());
+
+            Persona persona1 = new Persona(Nombre, Apellido, ID);
+
+            persona1.InfoPersona();
+
+            Console.ReadLine();
+
+            /*Console.WriteLine("Mostrar datos del usuario? (1 - SI / 2 - NO)" + Environment.NewLine);
+            int choice = Console.Read();
+
+            if (choice == 1)
+            {
+                
+            }*/
+        }
+    }
+}
